9406460e250a0fbd4ce55d4d8b5b0108632754a4
AntonyThorpe <antony.thorpe@budget12.com>
2017-06-16 18:26:52 +1200

    Updated webpack.config.js for bundling and minification
    Plus npm scripts for running the demo and test server

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
 // ./node_modules/webpack/bin/webpack.js
+const webpack = require('webpack');
+
+if (process.env.NODE_ENV === "production") {
+	var file_name_suffix = ".umd.bundle.min.js";
+} else {
+	var file_name_suffix = ".umd.bundle.js";
+}
 
 module.exports = {
-	entry: { 
-		demo: "./src/demo.js",
-		umd: "./src/createUMD.js"
+	entry: {
+		"apollo-client": "./src/apollo-client.js",
+		"subscription-transport-ws": "./src/subscription-transport-ws.js",
+		//onlinedemo: "./src/onlinedemo.js",  - need a new endpoint so skip
 	},
 	output: {
-		filename: "[name].bundle.js",
+		filename: "[name]" + file_name_suffix,
 		path: __dirname + '/dist',
 		libraryTarget: "umd",
 		umdNamedDefine: true
@@ -22,5 +30,12 @@ module.exports = {
 				]
 			}
 		}]
-	}
+	},
+	plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+	]
 };
